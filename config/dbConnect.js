46fa30c100acf74091546bf29764490e14e7516b
samikshk10 <88957313+samikshk10@users.noreply.github.com>
2023-08-05 18:31:02 +0545

    Update dbConnect.js

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,7 +1,6 @@
 const { MongoClient } = require('mongodb');
 
 const dbName = 'samik'; 
-// const url = 'mongodb+srv://samik:samik@cluster0.k1fxj5n.mongodb.net/?retryWrites=true&w=majority'; 
 const url = "mongodb://127.0.0.1:27017";
 
 let db;
